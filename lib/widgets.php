8aa2c1c1642bcdc4c3feea39cc58f66d8a8311cd
kfriedgen <katfcontact@gmail.com>
2018-04-30 00:16:36 -0400

    adding navigation styling and starting homepage layout

--- lib/widgets.php
+++ lib/widgets.php
@@ -16,4 +16,18 @@ function featured_content() {
 
 }
 add_action( 'widgets_init', 'featured_content' );
+
+function introduction() {
+
+    register_sidebar( array(
+        'name'          => 'Introduction',
+        'id'            => 'introduction',
+        'before_widget' => '<div class="introduction">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h4 class="introduction__title">',
+        'after_title'   => '</h4>',
+    ) );
+
+}
+add_action( 'widgets_init', 'introduction' );
 ?>
--- templates/header.php
+++ templates/header.php
@@ -1,27 +1,46 @@
 <header class="banner header">
 
-    <div class="container header__container">
+    <div class="container header__outer">
 
-      <div class="header__top">
+      <div class="header">
 
-          <a class="header__logo logo__a" href="<?= esc_url(home_url('/')); ?>">
-              <?php bloginfo('name'); ?>
-          </a>
+        <div class="header__inner header__logo-outer u-medium--hide">
 
-          <nav class="nav-primary header__navigation">
-              <?php
-              if (has_nav_menu('primary_navigation')) :
-                wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-              endif;
-              ?>
-          </nav>
+            <a class="header__logo logo__a" href="<?= esc_url(home_url('/')); ?>">
+                <?php bloginfo('name'); ?>
+            </a>
 
-          <div class="header__search">
-              <?php get_search_form(); ?>
-          </div>
+        </div>
+
+        <div class="header__inner header__navigation-outer">
+
+            <nav class="nav-primary header__navigation">
+                <?php
+                if (has_nav_menu('primary_navigation')) :
+                  wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+                endif;
+                ?>
+            </nav>
+
+        </div>
+
+        <div class="header__inner header__logo-outer u-medium--show">
+
+            <a class="header__logo logo__a" href="<?= esc_url(home_url('/')); ?>">
+                <?php bloginfo('name'); ?>
+            </a>
+
+        </div>
+
+        <div class="header__inner header__search-outer">
+
+            <div class="header__search">
+                <?php get_search_form(); ?>
+            </div>
+
+        </div>
 
       </div>
-
 
     </div>
 
