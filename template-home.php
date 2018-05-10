580116ffcd652d76cb43dfae5dfeec757e9dde0a
kfriedgen <katfcontact@gmail.com>
2018-05-09 23:17:14 -0400

    homepage featured sections

--- template-home.php
+++ template-home.php
@@ -8,6 +8,32 @@
 
     <?php get_template_part('templates/head'); ?>
 
-    <?php get_template_part('templates/home/introduction'); ?>
+    <div class="homepage">
+
+        <div class="homepage__inner">
+        
+            <div class="homepage__introduction">
+                <?php get_template_part('templates/home/introduction'); ?>
+            </div>
+
+        </div>
+
+        <div class="homepage__featured">
+
+            <div class="homepage__featured-inner">
+                <?php get_template_part('templates/home/featured-content'); ?>
+            </div>
+
+        </div>
+
+        <div class="homepage__secondary">
+            
+            <div class="homepage__secondary-inner">
+                <?php get_template_part('templates/home/recent-post'); ?>
+            </div>
+
+        </div>
+
+    </div>
     
 <?php endwhile; ?>
